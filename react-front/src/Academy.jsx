b7e1c73445b1de11cabd38c742d1215b0695dd60
RY <rehmetyeshanew@gmail.com>
2022-06-16 14:49:19 +0300

    added tailwindcss

---
+++ react-front/src/Academy.jsx
@@ -0,0 +1,13 @@
+function Academy() {
+    // const [count, setCount] = useState(0)
+  
+    return (
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">
+        Academy
+      </h1>
+      </div>
+    )
+  }
+  
+  export default Academy
---
+++ react-front/src/Trainee.jsx
@@ -0,0 +1,12 @@
+function Trainee() {
+ 
+    return (
+      <div className="App">
+        <h1 className="text-3xl font-bold underline">
+        Trainee
+      </h1>
+      </div>
+    )
+  }
+  
+  export default Trainee
