7a8b16d9a40401e8042228bab37235bacfb8f8e9
rehmet yeshanew <ryfgmz87@gmail.com>
2022-06-18 06:49:19 -0400

    structuring staff page

--- react-front/src/pages/StaffTransaction.jsx
+++ react-front/src/pages/StaffTransaction.jsx
@@ -7,6 +7,15 @@ import '../modal.css'
 
 const StaffTransaction = () => {
   const [time, setTime] = React.useState();
+
+  const todos = [
+    { id: 1, From: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", To: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY" , Message: "Sent to trainee J"},
+    { id: 2, From: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", To: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", Message: "Sent to trainee A" },
+    { id: 3, From: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", To: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", Message: "Sent to trainee K"},
+    { id: 1, From: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", To: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY" , Message: "Sent to trainee J"},
+    { id: 2, From: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", To: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", Message: "Sent to trainee A" },
+    { id: 3, From: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", To: "RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY", Message: "Sent to trainee K"},
+  ];
     
   React.useEffect(() => {
     const timer = setInterval(() => {
@@ -37,16 +46,18 @@ const StaffTransaction = () => {
         <h1 className='text-center text-4xl text-white mb-10'>Transactions</h1>
       </div>
 
-   
-    <div className='bg-red-900 w-80 rounded-lg relative mb-20'>
-        <div className='bg-red-500 ml-5 px-8 py-4 rounded'>           
-          
-                <div>
-                    {/* <h2 className='no mb-2'>From: RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY</h2>
-                    <h2 className='mb-2'>To: RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY</h2> */}
-                    <button className='bg-white p-3 rounded-full mb-10' onClick={toggleModal}>Details</button>
+    <section className='flex flex-wrap gap-16'>
+    {
+  todos.map((todo) => (   
+    <div key={todo.id} className='bg-red-900 w-80 rounded-lg relative mb-20'>
+        <div className='bg-red-500 ml-5 px-8 py-4 rounded w-80 '>           
+  
+                <div >
+    
+                    <button  className='bg-white p-3 rounded-full mb-10' onClick={toggleModal}>Details</button>
                     <h2 className='mb-2'>Amount: 0.001Algo</h2>
-                    <p className='mb-2'>Message: Sent to trainee J</p>
+                    <p className='mb-2'>Message: {todo.Message}</p>
+
                     <div className='mt-4 mb-5'>
                         <img src={logo} alt="" height={20} width={190} />
                     </div>
@@ -54,23 +65,28 @@ const StaffTransaction = () => {
                         <p>{time}</p>
                     </div>
 
-                    {modal && (
+    {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
-          <div className="modal-content">
-          <h2 className='mb-2'>From:  RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY</h2>
+          <div className="modal-content" >
+          <h2 className='mb-2'>From:{todo.From}</h2>
           <br />
-          <h2 className='mb-2'>To:  RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY</h2>
+          <h2 className='mb-2'>To: {todo.To}</h2>
             <button className="close-modal" onClick={toggleModal}>
               <AiFillCloseCircle size='28px'/>
             </button>
           </div>
         </div>
-      )}
+      )} 
+      
       </div>
-            
+      
         </div>
     </div>
+ ))  
+} 
+   
+    </section>
     </>
   )
 }
--- react-front/src/pages/StaffUpload.jsx
+++ react-front/src/pages/StaffUpload.jsx
@@ -8,25 +8,25 @@ const StaffUpload = () => {
       </div>   
    
     <div>
-        <div className='bg-red-500 p-2'>
+        <div className='p-2'>
              
-                <form action="" className='flex flex-wrap gap-40 m-10 p-5 justify-center bg-gray-800'>
+                <form action="" className='flex flex-wrap gap-40 m-10 p-5 py-20 justify-center bg-gray-800'>
+                    <div className='bg-white py-4 rounded-lg'>
+                    <div className='p-5 w-96 relative'>
+                        <input type="text" placeholder='trainee name' className='p-2 w-full border rounded hover:bg-gray-100' />
+                    </div>
+
+                    <div className='p-5'>
+                        <input type="text" placeholder='trainee email' className='p-2 w-full border rounded hover:bg-gray-100' />
+                    </div>
+
+                    <div className='p-5'>
+                        <input type="text" placeholder='trainee certificate' className='p-2 w-full border rounded hover:bg-gray-100' />
+                    </div>
+                    </div>
+
                     <div>
-                    <div className='p-5'>
-                        <input type="text" placeholder='trainee name' className='p-2 rounded hover:bg-gray-100' />
-                    </div>
-
-                    <div className='p-5'>
-                        <input type="text" placeholder='trainee email' className='p-2 rounded hover:bg-gray-100' />
-                    </div>
-
-                    <div className='p-5'>
-                        <input type="text" placeholder='trainee certificate' className='p-2 rounded hover:bg-gray-100' />
-                    </div>
-                    </div>
-
-                    <div>
-                        <button className='p-4 bg-red-600 px-10 rounded-full text-white'>Send</button>
+                        <button className='p-4 my-32 bg-red-600 px-10 rounded-full text-white'>Send</button>
                     </div>                    
                 </form>
             </div>
