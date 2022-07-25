63cf675bbf1b1ac34a04427981c46e7f71f270d5
netes <toygame8888@gmail.com>
2022-07-25 19:33:34 +0300

    data change

---
+++ react-front/src/Form.style.jsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const FormStyle = styled.input`
+    margin-bottom : 20px;
+    width : 200px;
+    padding : 10px;
+    border-radius : 5px;
+    border-color : #245EC7; `
--- react-front/src/deser.jsx
+++ react-front/src/deser.jsx
@@ -1,54 +1,72 @@
 import logo from './10logo.jpg';
 import pic from './10a.png';
-import React, { useContext,useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { MdMoney } from 'react-icons/md'; 
 import { AiFillCloseCircle } from 'react-icons/ai'
-import TraineeRequest from './pages/TraineeRequest';
+import StaffUpload from './pages/StaffUpload';
 import Transaction from './pages/Transaction';
+import axios from "axios";
+
+// import Aos from 'aos';
+// import "aos/dist/aos.css"
 
 import "./modal.css";
-// import TransactionContext from './context/TransactionContext';
 
-// const { connect } = useContext(TransactionContext);
-function connect() {
-  AlgoSigner.connect()
-  .then((d) => {
-    alert(JSON.stringify("Connected Successfully!", null, 2));
+// function connect() {
+//   AlgoSigner.connect()
+//   .then((d) => {
+//     alert(JSON.stringify("Connected Successfully!", null, 2));
+//   })
+//   .catch((e) => {
+//     console.error(e);
+//     alert(JSON.stringify(e, null, 2));
+//   })
+//   .finally(() => {
+//     hljs.highlightBlock(connectCodeElem);
+//     console.log('summer');
+//   });
+// }
 
-  })
-  .catch((e) => {
-    console.error(e);
-    alert(JSON.stringify(e, null, 2));
-  })
-  .finally(() => {
-     console.log('summer');
-  });
-}
+function Staff() {
+  const [initialData, setInitialData] = useState([{}]);
 
-function accounts() { 
-  AlgoSigner.accounts({
-    ledger: 'TestNet'
-  })
-  .then((d) => {
-    const arr = JSON.stringify(d);
-    alert(arr.current);
-    // const account = arr.find(obj => {
-    //   return obj.address;
-    // });
-    // this.setCurrentAccount(arr);
-  })
-  .catch((e) => {
-    console.error(e);
-   })
-  .finally(() => {
-  });
-}
+  // function getData() {
+  //   axios({
+  //     method: "GET",
+  //     url:"http://localhost:5000/api",
+  //   })
+  //   .then((response) => {
+  //     const res =response.data
+  //     console.log(res);
+  //     // setProfileData(({
+  //     //   profile_name: res.name,
+  //     //   about_me: res.about}))
+  //   }).catch((error) => {
+  //     if (error.response) {
+  //       console.log(error.response)
+  //       console.log(error.response.status)
+  //       console.log(error.response.headers)
+  //       }
+  //   })}
+  // const userAccount = useRef();
+const connectAlgoSigner = async () =>{
+     await AlgoSigner.connect();
+    // console.log(resp)
+    // getUserAccount();
+  }
 
-function Trainee() {
-  // const { connect } = useContext(TransactionContext);
+  const getUserAccount = async () =>{
+     let userAccount =  await AlgoSigner.accounts({
+          ledger: 'TestNet'
+        })
+  // console.log(userAccount.current[0]['address'])
+  let data = JSON.stringify(userAccount);
+  alert(JSON.stringify(userAccount))
+
+  }
 
     const home = useRef(null);
-    const request = useRef(null);
+    const form = useRef(null);
     const transaction = useRef(null);
     
 
@@ -77,8 +95,8 @@ function Trainee() {
                 <li onClick={()=> scrollToSection(home)}  className="text-white ml-8 hover:bg-red-500 hover:filter hover:brightness-110 hover:transition hover:ease-in-out cursor-pointer rounded px-5 py-2">
                    Home
                 </li>
-                <li onClick={()=> scrollToSection(request)} className='text-white ml-8 hover:bg-red-500 hover:filter hover:brightness-110 hover:transition hover:ease-in-out cursor-pointer rounded px-5 py-2'>
-                   Request
+                <li onClick={()=> scrollToSection(form)} className='text-white ml-8 hover:bg-red-500 hover:filter hover:brightness-110 hover:transition hover:ease-in-out cursor-pointer rounded px-5 py-2'>
+                   Form
                 </li>
                 <li onClick={()=> scrollToSection(transaction)} className='text-white ml-8 hover:bg-red-500 hover:filter hover:brightness-110 hover:transition hover:ease-in-out cursor-pointer rounded px-5 py-2'>
                     Transaction
@@ -86,12 +104,12 @@ function Trainee() {
             </ul>
         </nav>
        
-      <div ref={home} className="mt-32 flex flex-row flex-wrap gap-52 justify-center text-white">
+      <div ref={home} className="flex flex-row flex-wrap gap-52 justify-center mt-32 text-white">
       <div>
       
       <div className='mr-96 mb-10 flex gap-5 flex-row-reverse'>
-      <h1 class Name="text-3xl">
-        10academy
+      <h1 className="text-3xl">
+       <button>10academy</button> 
       </h1><span><img src={logo} alt="" height={20} width={70} /></span>
       </div>
      
@@ -100,9 +118,9 @@ function Trainee() {
         <p>10 Academy identifies, trains and enables exceptionally talented young Africans <br></br> to have an outsized impact on the world.</p>
       </div>
 
-      <div><img src={pic} alt="" height={10} width={490} /></div>
+      <div><img src={pic} alt="" height={30} width={490} /></div>
       <div className='mt-10'>
-        <a href="" onClick={connect} className='text-md bg-red-600 p-4 rounded-full text-white hover:filter hover:brightness-110'>Connect Wallet</a>
+        <a href="" onClick={connectAlgoSigner} className='text-md bg-red-600 p-4 rounded-full text-white hover:filter hover:brightness-110'>Connect Wallet</a>
       </div>
       </div>
       
@@ -113,17 +131,17 @@ function Trainee() {
           <div><MdMoney size="40px" /></div>
           <div className='flex flex-col justify-center text-center mt-8 text-xl'>
           <h1>Amount: 5 Algod</h1>
-          <div><p>Algorand</p></div>
+          <div><p>userAccount</p></div>
           </div>
           
           <div className='absolute mt-12'>
-            <button className='bg-gradient-to-l from-blue-600 via-gray-900 to-blue-600 p-2 text-white rounded-full hover:filer hover:brightness-110' onClick={accounts}>Get Public key</button>
+            <button className='bg-gradient-to-l from-blue-600 via-gray-900 to-blue-600 p-2 text-white rounded-full hover:filer hover:brightness-110' onClick={getUserAccount}>Get Public key</button>
           </div>
           {modal && (
         <div className="modal">
           <div onClick={toggleModal} className="overlay"></div>
           <div className="modal-content">
-            <h2 class="text-gray-900 text-center">
+            <h2 className="text-gray-900 text-center">
             RBAWCKKRQQSP5HVTSSSZSMWZZFKREGYMXIJ6PDERWGVTCSZCHAJZB76JKY
             </h2>
             <button className="close-modal" onClick={toggleModal}>
@@ -137,8 +155,8 @@ function Trainee() {
       </div>
       
       <div className='flex flex-col flex-wrap mx-20 gap-20'>
-      <div ref={request}>
-      <TraineeRequest />
+      <div ref={form}>
+      <StaffUpload />
       </div>
 
       <div ref={transaction}>
@@ -149,4 +167,4 @@ function Trainee() {
     )
   }
   
-  export default Trainee
+  export default Staff
