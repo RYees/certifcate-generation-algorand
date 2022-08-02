2272d52a0b12f05e19f01389f8cd2e89a21900c9
netes <toygame8888@gmail.com>
2022-08-02 18:22:00 +0300

    data changing

--- my-app/src/components/AlgoSigner.styles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const AlgoSignerMain = styled.div`
-    margin-top : 20px;
-    @media(max-width : 767px) {
-        width : 90%;
-        margin : 36px auto;
-    }
-`
